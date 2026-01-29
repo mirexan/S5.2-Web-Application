6c0f062e4dfbe8e688012d2a955ecb561be94999
mirexan <mmregada@gmail.com>
2026-01-29 11:37:35 +0100

    chore: created boticarium front directories

---
+++ boticarium-front/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Bienvenido a Boticarium</h1>
+      <p>Frontend conectado... esperando instrucciones.</p>
+    </div>
+  )
+}
+
+export default App
